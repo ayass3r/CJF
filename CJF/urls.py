503e1cfcc6b1240439b02129bf1c37e96a1b7094
oyn <omar@Omars-MacBook-Pro-8.local>
2015-01-28 16:53:03 +0200

    Program Page Template Done

--- CJF/urls.py
+++ CJF/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = patterns('',
   
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^artists/', include('LineUp.urls')),
+    url(r'^program/', include('LineUp.urls')),
     url(r'^home/', include('Carousel.urls')),
     url(r'^specialguests/', include('SpecialGuests.urls')),
 
--- LineUp/views.py
+++ LineUp/views.py
@@ -4,4 +4,4 @@ from LineUp.models import Artist
 # Create your views here.
 def index(request):
     artistList = Artist.objects.all().order_by('ArtistName')
-    return render(request, 'LineUp/index.html', {'artists':artistList})
+    return render(request, 'LineUp/program.html', {'artists':artistList})
