da77ffc49d57bb2e3e8af79a3ca5215db9f344f4
ayass3r <ayass3r@aucegypt.edu>
2015-02-10 14:07:49 +0200

    Home Page and Sponsors Page Partial

--- PreviousEvents/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from PreviousEvents.models import Youtube, SoundCloud, EventPicture
-
-admin.site.register(Youtube)
-
-admin.site.register(SoundCloud)
-
-admin.site.register(EventPicture)
